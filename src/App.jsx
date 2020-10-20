c4a3e43de1601db951cfb6d79f425e56a43da117
norberdo <norbert.suski@gmail.com>
2020-10-20 19:48:54 +0200

    feat: form added to appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>Learn Redux</p>
-    </header>
-  </div>
-);
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoTitle, setTodoTitle] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos((prevTodos) => [...todos, { title: todoTitle, id: prevTodos.length }]);
+    setTodoTitle('');
+  };
+
+  const handleOnChange = (event) => {
+    setTodoTitle(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">Learn Redux</header>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input value={todoTitle} type="text" name="title" onChange={handleOnChange} />
+          <button type="submit">Submit</button>
+        </form>
+
+        <p>List of TODOs:</p>
+        <ul>
+          {todos.map(({ id, title }) => (
+            <li key={id}>{title}</li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
 
 export default App;
